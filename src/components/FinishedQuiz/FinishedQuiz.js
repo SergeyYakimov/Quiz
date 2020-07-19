efb5a489a46a725b4989a41e9f7f9fed9c9f1407
SergeyYakimov <teriertis@yandex.ru>
2020-07-19 23:04:21 +0800

    Made Button component

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import classes from './FinishedQuiz.module.css'
+import Button from '../UI/Button/Button';
+import classes from './FinishedQuiz.module.css';
 
 const FinishedQuiz = props => {
   const successCount = Object
@@ -32,7 +33,8 @@ const FinishedQuiz = props => {
       <p>Правильно {successCount} из {props.quiz.length}</p>
       
       <div>
-        <button onClick={props.onRetry}>Повторить</button>
+        <Button onClick={props.onRetry} type={'primary'}>Повторить</Button>
+        <Button onClick={() => {}} type={'success'}>Перейти в список тестов</Button>
       </div>
     </div>
   )
