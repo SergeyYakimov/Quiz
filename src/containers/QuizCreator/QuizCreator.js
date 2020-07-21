4a85878e40eb4a578ae05aaedb283fc7ee133746
SergeyYakimov <teriertis@yandex.ru>
2020-07-22 00:30:18 +0800

    Made pages

---
+++ src/containers/QuizCreator/QuizCreator.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Quiz Creator</h1>
+      </div>
+    )
+  }
+}
+
